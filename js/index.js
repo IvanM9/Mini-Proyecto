2cf8d5887981cf1c1ef53f24b2439e8a5fabed02
Iván Manzaba <mauricio.9.inm@gmail.com>
2021-12-25 17:43:38 -0500

    Correciones de texto, alertas añadidas, eventos agregados

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
     // Evento click del botón ingresar
     $("#ingresar").click(function () {
+        ingresar();
+    });
+    // Evento keydown para ingresar
+    $("input").keyup(function (event) { 
+        if(event.which == "13")
+            ingresar();
+    });
+
+    // Se hace el procedimiento de validación y se redirige 
+    // a la siguiente página
+    function ingresar() {
         let datos = {
             "correo": $("#inputEmail").val(),
             "contraseña": $("#InputPassword").val()
@@ -10,12 +21,13 @@ $(document).ready(function () {
             if (datos.correo === "prueba@gmail.com" && datos.contraseña === "54321")
                 location.href = "/plantillas/inicio.html"
             else
-                alert("Datos incorrectos");
+                viewErorr("Datos incorrectos","Intente nuevamente o solicite recordar las credenciales")
+            
+                
         }
         else
-            alert("Hay campos vacios");
-    });
-
+            viewErorr("Hay campos vacios", "Llene todos los campos");
+    }
     // Se verifica que los campos no estén en blanco
     function verificarCamposVacios(datos) {
         return datos.correo.trim() !== "" && datos.contraseña.trim() !== "";
@@ -23,7 +35,17 @@ $(document).ready(function () {
 
     // Evento click para obtener las credenciales de acceso 
     $("#credenciales").click(function () {
-        alert("Correo: prueba@gmail.com, Contraseña: 54321");
-
+        Swal.fire(
+            "Correo: prueba@gmail.com",
+            "Contraseña: 54321"
+        );
     });
+
+    const viewErorr = (titulo, texto) => {
+        Swal.fire({
+            icon: 'error',
+            title: titulo,
+            text: texto
+        });
+    }
 });
--- js/inicio.js
+++ js/inicio.js
@@ -1,3 +1,99 @@
-$(document).ready(function () {
+$().ready(function () {
+
+    // Eventos de los botones
+    $("#boton1").click(function () {
+        addCart("boton1");
+    });
+    $("#boton2").click(function () {
+        addCart("boton2");
+    });
+    $("#boton3").click(function () {
+        addCart("boton3");
+    });
+    $("#boton4").click(function () {
+        addCart("boton4");
+    });
+    $("#boton5").click(function () {
+        addCart("boton5");
+    });
+    $("#boton6").click(function () {
+        addCart("boton6");
+    });
+    $("#compra").click(function () {
+        let cantProductos = $("#carrito").text();
+        if (cantProductos >0) {
+            Swal.fire({
+                title: 'Tiene ' +cantProductos + " productos en el carrito",
+                text: "¿Desea continuar con la compra?",
+                icon: 'info',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Sí'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        'Su compra está en camino',
+                        '¡Gracias por confiar en nosotros!'
+                    )
+                }
     
+            });
+        }
+        else{
+            Swal.fire("No tiene productos en el carrito");
+        }
+    });
+    $("#calificacion").click(function () {
+        showValoracion();
+    });
+    $("#comentario").click(function () { 
+        showValoracion();
+        
+    });
+
+    // Función para añadir productos al carrito
+    function addCart(boton) {
+        let cant = "#cantidad" + boton.slice(5, 6);
+        let ncant = $(cant).val();
+        if (ncant > 0) {
+            let newcant = parseInt($("#carrito").text()) + parseInt(ncant);
+            $("#carrito").text(newcant);
+            addSuccessful();
+        }
+        else
+            showError();
+    }
+
+    // Mensaje por la valoración de la página
+    function showValoracion() {
+        Swal.fire(
+            {
+                icon: 'success',
+                title: 'Gracias por su valoración'
+            }
+        );
+
+    }
+
+    // Mensaje de error
+    function showError() {
+        Swal.fire({
+            icon: 'error',
+            title: 'No se ha podido añadir',
+            text: 'La cantidad de productos no es correcta'
+        });
+    }
+
+    // Mensaje de correcto
+    function addSuccessful() {
+        Swal.fire(
+            {
+                icon: 'success',
+                title: 'Añadido correctamente'
+            }
+        );
+    }
+
+
 });
